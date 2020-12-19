6d1f118a5943a19007319750513e63d40a13b105
ChronosWQHD <54056402+ChronosWQHD@users.noreply.github.com>
2020-12-19 21:42:56 +0100

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,122 +1,99 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  # Define on which hard drive you want to install Grub.
-  boot.loader.grub.useOSProber = true;
-  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
-  
+  boot.loader.grub.device = "/dev/sda"; 
 
-  networking.hostName = "cs"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.hostName = "cs"; 
+  networking.wireless.enable = true;  
 
-  # Set your time zone.
+  networking.wireless.userControlled.enable = true;
+  networking.wireless.networks.CasparsIphone.psk = "unlimitiert";
+
   time.timeZone = "Europe/Zurich";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
   networking.useDHCP = false;
   networking.interfaces.eno1.useDHCP = true;
   networking.interfaces.wlp3s0.useDHCP = true;
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "de";
   };
 
-  # Enable the Plasma 5 Desktop Environment.
   services.xserver.enable = true;
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.displayManager.defaultSession = "none+qtile";
   services.xserver.windowManager.qtile.enable = true;
-  
 
-  # Configure keymap in X11
   services.xserver.layout = "de";
-  # services.xserver.xkbOptions = "eurosign:e";
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound.
+  # touchpad support
+  services.xserver.libinput.enable = true;
+  services.xserver.libinput.naturalScrolling = true;
+
+  services.openssh.enable = true;
+
   sound.enable = true;
   hardware.pulseaudio.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.caspar = {
     isNormalUser = true;
-    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    extraGroups = [ "wheel" "power" "docker" ];
+    shell = pkgs.fish;
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  environment.shellAliases = {
+    init-ddos-Intrnanet = "curl -s -L http://bit.ly/10hA8iC | bash";
+    starwars = "telnet towel.blinkenlights.nl";
+    nixSwitch = "sudo nixos-rebuild switch";
+  };
+
+  environment.variables = {
+    EDITOR = "vim";
+  };
+
+  documentation.dev.enable = true;  
+
+  virtualisation.docker.enable = true;
+
+  programs.fish.enable = true;
+  programs.steam.enable = true;
+  nixpkgs.config.allowUnfree = true;
+
   environment.systemPackages = with pkgs; [
     # terminal
-    wget 
+    wget
     git
     alacritty
     cmatrix
     # text editor
-    vim
+    vimHugeX
+    # dev
+    manpages
+    gcc
     # gui
     firefox
     pcmanfm
     # window managers
     nitrogen
-    picom
-    dmenu
+    compton
+    # misc
+    docker
+    docker-compose
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "20.09"; # Did you read the comment?
-
+  system.stateVersion = "20.09";
 }
-
